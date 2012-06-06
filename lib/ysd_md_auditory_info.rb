d5347747b0bbe5a5c4418df76fcc6de869dbbcd9
yurak <yurak.sisa.dream@gmail.com>
2012-06-06 09:22:48 +0200

    first commit

---
+++ lib/ysd_md_auditory_info.rb
@@ -0,0 +1,40 @@
+module Auditory
+  #
+  # Extends a model adding auditory info
+  #
+  module AuditoryInfo
+
+    def self.included(model)
+
+      model.property :creation_date, DateTime, :field => 'creation_date' # The creation date
+      model.property :creation_user, DateTime, :field => 'creation_user' # The user who created it
+      model.property :last_update, DateTime, :field => 'last_update'     # If the content has been modified it holds the last update date
+      model.property :last_update_user, String, :field => 'last_update_user', :length => 20 # The user who updated it
+    
+    end
+
+    # Overwritten to store auditory data
+    #
+    def create
+     
+      attribute_set(:creation_date, Time.now) if (attribute_get(:creation_date).nil? or not attribute_get(:creation_date) or attribute_get(:creation_date).to_s.strip.length == 0)
+      
+      if Auditory::AuditoryInfo.method_defined?(:connected_user) 
+        attribute_set(:creation_user, connected_user.username)
+      end
+      super
+    end 
+
+    # Overwritten to store auditory data
+    #
+    def update
+      attribute_set(:last_update, Time.now)
+      if Auditory::AuditoryInfo.method_defined?(:connected_user)
+        attribute_set(:last_update_user, connected_user.username)
+      end
+      
+      super
+    end
+
+  end #AuditoryInfo
+end #Auditory
---
+++ ysd-md-auditory.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+  s.name    = "ysd_md_auditory"
+  s.version = "0.1"
+  s.authors = ["Yurak Sisa Dream"]
+  s.date    = "2012-03-16"
+  s.email   = ["yurak.sisa.dream@gmail.com"]
+  s.files   = Dir['lib/**/*.rb','templates/**/*.erb']
+  s.summary = "Model extension for auditory"
+  
+end
